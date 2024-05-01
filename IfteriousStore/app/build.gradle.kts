767c0a3f30e8e8630748a1b8988e542674a31bd0
Ifham Ahmed Khan <ifham.khan105@gmail.com>
2024-05-01 18:18:24 +0500

    created login and main page

--- IfteriousStore/app/build.gradle.kts
+++ IfteriousStore/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.ifterious_store"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.ifterious_store"
         minSdk = 16
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
---
+++ IfteriousStore/app/src/main/java/com/example/ifterious_store/HomeActivity.java
@@ -0,0 +1,33 @@
+package com.example.ifterious_store;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+    Button button,button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, Signup.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
