5ca36403896d18540e27f17e41be6330e8b7d2fd
Ifham Ahmed Khan <ifham.khan105@gmail.com>
2024-05-03 10:31:17 +0500

    profile page and set profile created

--- IfteriousStore/app/src/main/java/com/example/ifterious_store/Profile.java
+++ IfteriousStore/app/src/main/java/com/example/ifterious_store/Profile.java
@@ -2,13 +2,51 @@ package com.example.ifterious_store;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Profile extends AppCompatActivity {
+    TextView textView7,textView8,textView9;
+    Button button14;
+    SharedPreferences sharedPreferences;
 
+    private static final String SHARED_PREF_NAME = "mypref";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_EMAIL = "email";
+    private static final String KEY_PHONE = "phone";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        textView7 = findViewById(R.id.textView7);
+        textView8 = findViewById(R.id.textView8);
+        textView9 = findViewById(R.id.textView9);
+        button14 = findViewById(R.id.button14);
+        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME,MODE_PRIVATE);
+        String name = sharedPreferences.getString(KEY_NAME,null);
+        String email = sharedPreferences.getString(KEY_EMAIL,null);
+        String phone = sharedPreferences.getString(KEY_PHONE,null);
+        if(name != null || email != null || phone != null){
+            textView7.setText("Username: "+name);
+            textView8.setText("Email: "+email);
+            textView9.setText("Phone: "+phone);
+        }
+        button14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Profile.this,Set_Profile.class);
+                startActivity(intent);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.commit();
+                finish();
+                Toast.makeText(Profile.this, "Editing profile", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ IfteriousStore/app/src/main/java/com/example/ifterious_store/Set_Profile.java
@@ -0,0 +1,47 @@
+package com.example.ifterious_store;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Set_Profile extends AppCompatActivity {
+    EditText editTextTextPersonName3,editTextTextEmailAddress3,editTextPhone2;
+    Button button15;
+    SharedPreferences sharedPreferences;
+
+    private static final String SHARED_PREF_NAME = "mypref";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_EMAIL = "email";
+    private static final String KEY_PHONE = "phone";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_profile);
+        editTextTextPersonName3 = findViewById(R.id.editTextTextPersonName3);
+        editTextTextEmailAddress3 = findViewById(R.id.editTextTextEmailAddress3);
+        editTextPhone2 = findViewById(R.id.editTextPhone2);
+        button15 = findViewById(R.id.button15);
+        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+        button15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(KEY_NAME,editTextTextPersonName3.getText().toString());
+                editor.putString(KEY_EMAIL,editTextTextEmailAddress3.getText().toString());
+                editor.putString(KEY_PHONE,editTextPhone2.getText().toString());
+                editor.apply();
+
+                Intent intent = new Intent(Set_Profile.this,Profile.class);
+                startActivity(intent);
+
+                Toast.makeText(Set_Profile.this, "Details saved successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
