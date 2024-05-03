03b627ed863cb6f3b959a3adaef24af239d14e25
Ifham Ahmed Khan <ifham.khan105@gmail.com>
2024-05-03 10:20:11 +0500

    deals page added

---
+++ IfteriousStore/app/src/main/java/com/example/ifterious_store/Deal_Five.java
@@ -0,0 +1,14 @@
+package com.example.ifterious_store;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Deal_Five extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deal_five);
+    }
+}
--- IfteriousStore/app/src/main/java/com/example/ifterious_store/Login.java
+++ IfteriousStore/app/src/main/java/com/example/ifterious_store/Login.java
@@ -2,13 +2,41 @@ package com.example.ifterious_store;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
-
+    EditText editTextTextEmailAddress;
+    EditText editTextTextPassword;
+    Button button3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        editTextTextEmailAddress = findViewById(R.id.editTextTextEmailAddress);
+        editTextTextPassword = findViewById(R.id.editTextTextPassword);
+        button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email= editTextTextEmailAddress.getText().toString();
+                String password = editTextTextPassword.getText().toString();
+                if(TextUtils.isEmpty(email)){
+                    editTextTextEmailAddress.setError("Please enter email to login");
+                }
+                else if(TextUtils.isEmpty(password)){
+                    editTextTextPassword.setError("Please enter password to login");
+                }
+                else{
+                    Intent intent = new Intent(Login.this, Home_page.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
