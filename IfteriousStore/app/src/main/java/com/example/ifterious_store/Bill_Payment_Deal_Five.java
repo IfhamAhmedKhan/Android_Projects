7a696e13743d15cb5bd03e4bb56af8473bc35a48
Ifham Ahmed Khan <ifham.khan105@gmail.com>
2024-05-03 10:43:47 +0500

    deal and billing page created

---
+++ IfteriousStore/app/src/main/java/com/example/ifterious_store/Bill_Payment_Deal_Five.java
@@ -0,0 +1,43 @@
+package com.example.ifterious_store;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Bill_Payment_Deal_Five extends AppCompatActivity {
+    EditText editTextTextPersonName2;
+    EditText editTextTextPostalAddress;
+    EditText editTextPhone;
+    Button button12;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bill_payment_deal_five);
+        editTextTextPersonName2 = findViewById(R.id.editTextTextPersonName2);
+        editTextTextPostalAddress =findViewById(R.id.editTextTextPostalAddress);
+        editTextPhone = findViewById(R.id.editTextPhone);
+        button12 = findViewById(R.id.button12);
+        button12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = editTextTextPersonName2.getText().toString();
+                String address = editTextTextPostalAddress.getText().toString();
+                String phone = editTextPhone.getText().toString();
+                if (TextUtils.isEmpty(username)) {
+                    editTextTextPersonName2.setError("Please enter your username");
+                } else if (TextUtils.isEmpty(address)) {
+                    editTextTextPostalAddress.setError("Please enter your address");
+                } else if (TextUtils.isEmpty(phone)) {
+                    editTextPhone.setError("Please enter your phone number");
+                } else {
+                    Toast.makeText(Bill_Payment_Deal_Five.this, "Your order is confirmed "+username, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
