36ce8aab56b866eda5c352c8e647505f3c65304a
Ifham Ahmed Khan <ifham.khan105@gmail.com>
2024-05-05 21:16:57 +0500

    updated and fix some errors

--- IfteriousStore/app/src/main/java/com/example/ifterious_store/Bill_Payment_Deal_Six.java
+++ IfteriousStore/app/src/main/java/com/example/ifterious_store/Bill_Payment_Deal_Six.java
@@ -14,12 +14,13 @@ public class Bill_Payment_Deal_Six extends AppCompatActivity {
     EditText editTextTextPostalAddress;
     EditText editTextPhone;
     Button button12;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bill_payment_deal_six);
         editTextTextPersonName2 = findViewById(R.id.editTextTextPersonName2);
-        editTextTextPostalAddress =findViewById(R.id.editTextTextPostalAddress);
+        editTextTextPostalAddress = findViewById(R.id.editTextTextPostalAddress);
         editTextPhone = findViewById(R.id.editTextPhone);
         button12 = findViewById(R.id.button12);
         button12.setOnClickListener(new View.OnClickListener() {
@@ -35,8 +36,9 @@ public class Bill_Payment_Deal_Six extends AppCompatActivity {
                 } else if (TextUtils.isEmpty(phone)) {
                     editTextPhone.setError("Please enter your phone number");
                 } else {
-                    Toast.makeText(Bill_Payment_Deal_Six.this, "Your order is confirmed "+username, Toast.LENGTH_LONG).show();
+                    Toast.makeText(Bill_Payment_Deal_Six.this, "Your order is confirmed " + username, Toast.LENGTH_LONG).show();
                 }
             }
         });
     }
+}
